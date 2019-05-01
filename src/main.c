34e47568ad7d8b67e50a356710086a39a4af1cc5
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2019-05-01 20:40:21 -0300

    Make explicit when arguments are passed by reference

--- src/main.c
+++ src/main.c
@@ -11,11 +11,11 @@ typedef struct {
   int y;
 } player_t;
 
-void read_level(char dest_board[], char curr_board[], int y_loc[],
-                int *y_height, player_t *player) {
+void read_level(char (*dest_board)[], char (*curr_board)[], int (*y_loc)[],
+                int *y_height, player_t **player) {
+  int row = 0;
+  int col = 0;
   int board_idx = 0;
-  int r = 0;
-  int c = 0;
 
   char buffer[BUFFER_SIZE];
   while (fgets(buffer, sizeof(buffer), stdin)) {
@@ -23,40 +23,40 @@ void read_level(char dest_board[], char curr_board[], int y_loc[],
       if (buffer[buffer_idx] == '\0')
         break;
       else if (buffer[buffer_idx] == '\n') {
-        c = 0;
-        r++;
+        col = 0;
+        row++;
         break;
       }
 
-      if (c == 0) {
-        y_loc[r] = board_idx;
-        *y_height = r + 1;
+      if (col == 0) {
+        (*y_loc)[row] = board_idx;
+        *y_height = row + 1;
       }
 
       char ch = buffer[buffer_idx];
-      dest_board[board_idx] = ch != '$' && ch != '@' ? ch : ' ';
-      curr_board[board_idx] = ch != '.' ? ch : ' ';
+      (*dest_board)[board_idx] = ch != '$' && ch != '@' ? ch : ' ';
+      (*curr_board)[board_idx] = ch != '.' ? ch : ' ';
 
       if (ch == '@') {
-        player->x = c;
-        player->y = r;
+        (*player)->x = col;
+        (*player)->y = row;
       }
 
-      c++;
+      col++;
       board_idx++;
     }
   }
 
-  dest_board[board_idx] = '\0';
-  curr_board[board_idx] = '\0';
+  (*dest_board)[board_idx] = '\0';
+  (*curr_board)[board_idx] = '\0';
 }
 
-void move(char *moved_board[], int y_loc[], int x, int y, int dx, int dy,
+void move(char (*moved_board)[], int y_loc[], int x, int y, int dx, int dy,
           char trial_board[]) {
   int new_player_idx = y_loc[y + dy] + x + dx;
 
   if (trial_board[new_player_idx] != ' ')
-    *moved_board = NULL;
+    (*moved_board)[0] = '\0';
   else {
     strcpy(*moved_board, trial_board);
     (*moved_board)[y_loc[y] + x] = ' ';
@@ -69,28 +69,27 @@ int main(void) {
   int y_loc[BOARD_SIZE], y_height;
   player_t *player;
 
-  player = malloc(sizeof(player));
+  player = malloc(sizeof(player_t));
 
-  read_level(dest_board, curr_board, y_loc, &y_height, player);
+  read_level(&dest_board, &curr_board, &y_loc, &y_height, &player);
+
 #ifdef DEBUG
-  char *expected_dest_board =
-      "########     ##     ##. #  ##.    ##.    ##.#   ########";
-  assert(strcmp(dest_board, expected_dest_board) == 0);
-
-  char *expected_curr_board =
-      "########     ##     ##  #  ##  $$ ## $$  ## #  @########";
-  assert(strcmp(curr_board, expected_curr_board) == 0);
+  assert(0 ==
+         strcmp(dest_board,
+                "########     ##     ##. #  ##.    ##.    ##.#   ########"));
+  assert(0 ==
+         strcmp(curr_board,
+                "########     ##     ##  #  ##  $$ ## $$  ## #  @########"));
 
   assert(player->x == 5);
   assert(player->y == 6);
 
-  for (int i = 0; i < y_height; i++)
-    assert(y_loc[i] == i * 7);
+  for (int row = 0; row < y_height; row++)
+    assert(y_loc[row] == row * 7);
 #endif
 
 #ifdef DEBUG
-  char *moved_board;
-  moved_board = malloc(BOARD_SIZE);
+  char moved_board[BOARD_SIZE];
 
   move(&moved_board, y_loc, player->x, player->y, 0, -1, curr_board);
   assert(strcmp(moved_board,
@@ -98,8 +97,18 @@ int main(void) {
          0);
 
   move(&moved_board, y_loc, player->x, player->y, 1, 0, curr_board);
-  assert(moved_board == NULL);
+  assert(strcmp(moved_board, "") == 0);
+
+  move(&moved_board, y_loc, player->x, player->y, 0, 1, curr_board);
+  assert(strcmp(moved_board, "") == 0);
+
+  move(&moved_board, y_loc, player->x, player->y, -1, 0, curr_board);
+  assert(strcmp(moved_board,
+                "########     ##     ##  #  ##  $$ ## $$  ## # @ ########") ==
+         0);
 #endif
+
+  free(player);
 
   return 0;
 }
